18c9250554e2174e16fa2660849bfc64f9348028
wandersonsangreal <sangreal.wanderson@hotmail.com>
2022-04-09 17:39:59 -0300

    Modelos iniciais de Category, Genre e Castmember

---
+++ src/main/java/com/fullcycle/FCCatalogo/FcCatalogoApplication.java
@@ -0,0 +1,27 @@
+package com.fullcycle.FCCatalogo;
+
+import com.fullcycle.FCCatalogo.domain.entity.Category;
+import com.fullcycle.FCCatalogo.domain.entity.Genre;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcCatalogoApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcCatalogoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Genre genre = new Genre("Genre 1");
+		Category category = new Category("Category 1");
+		genre.addCategory(category);
+
+		System.out.println("Genere name: " + genre.getId() + " " + genre.getName() + " " + genre.getCategories().get(0).getName());
+	}
+
+}
